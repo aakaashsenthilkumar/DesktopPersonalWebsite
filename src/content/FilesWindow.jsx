5a18057fa872a3fcfa7df96f2db9169c0239a7fe
aakaashsenthilkumar <aakaashtech@gmail.com>
2025-11-27 02:37:45 -0800

    Image fix

--- src/content/FilesWindow.jsx
+++ src/content/FilesWindow.jsx
@@ -1,3 +1,7 @@
+import resumePDF from "../assets/Resume_Aakaash_Senthilkumar.pdf";
+import transcriptPDF from "../assets/Aakaash_Student_Transcript_2025.pdf";
+import profileIMG from "../assets/boy-with-glasses.png";
+
 export default function FilesWindow() {
   return (
     <div className="content-container">
@@ -6,39 +10,29 @@ export default function FilesWindow() {
       <div className="file">
         <h3 className="file-title">Resume</h3>
         <p className="file-type">PDF</p>
-        <a
-          href="src/assets/Resume_Aakaash_Senthilkumar.pdf"
-          download
-          className="file-download"
-        >
-          Download → 
+        <a href={resumePDF} download className="file-download">
+          Download →
         </a>
       </div>
 
       <div className="file">
         <h3 className="file-title">Transcript</h3>
         <p className="file-type">PDF</p>
-        <a
-          href="src/assets/Aakaash_Student_Transcript_2025.pdf"
-          download
-          className="file-download"
-        >
-          Download → 
+        <a href={transcriptPDF} download className="file-download">
+          Download →
         </a>
       </div>
 
       <div className="file">
         <h3 className="file-title">Profile Image</h3>
-        <p className="file-type">JPEG/PNG</p>
-        <a
-          href="/files/profile.jpg"
-          download
-          className="file-download"
-        >
-          Download → 
+        <p className="file-type">PNG</p>
+
+        <a href={profileIMG} download className="file-download">
+          Download →
         </a>
-        <div className="file-preview">
-          <img src="src/assets/boy-with-glasses.png" alt="Profile" className="preview-img" />
+
+        <div className="file-preview" style={{ marginTop: "10px" }}>
+          <img src={profileIMG} alt="Profile" className="preview-img" style={{ width: "120px", borderRadius: "8px" }} />
         </div>
       </div>
     </div>
